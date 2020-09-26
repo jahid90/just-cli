7315e84df2bedd8594301b8cfb765ba2bd71038e
Jahid <s.jahid.90@gmail.com>
2020-09-26 21:20:28 +0530

    add cli name and description

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"gopkg.in/urfave/cli.v2"
 )
 
 func main() {
-	(&cli.App{}).Run(os.Args)
+	app := &cli.App{
+		Name:  "just",
+		Usage: "A command runner that runs commands defined in a config file (Justfile by default)",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Show usage here...")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
