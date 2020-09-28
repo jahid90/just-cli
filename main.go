82a12832f590a72fedc18454f789b67372ff2f5d
Jahid <s.jahid.90@gmail.com>
2020-09-28 21:19:34 +0530

    remove default action for main command; displays usage

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -13,10 +12,6 @@ func main() {
 	app := &cli.App{
 		Name:  "just",
 		Usage: "A command runner that runs commands defined in a config file (Justfile by default)",
-		Action: func(c *cli.Context) error {
-			fmt.Println("Show usage here...")
-			return nil
-		},
 	}
 
 	app.Commands = hello.GetCommands()
