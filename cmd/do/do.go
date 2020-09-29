ced9b25dabde578186a6391f2545b77c4c230143
Jahid <s.jahid.90@gmail.com>
2020-09-29 10:39:39 +0530

    add capability to execute arbitrary commands via "just do"

--- cmd/do/do.go
+++ cmd/do/do.go
@@ -2,9 +2,20 @@ package do
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/urfave/cli/v2"
 )
+
+func runCommand(cmd string, args ...string) ([]byte, error) {
+	out, err := exec.Command(cmd, args...).Output()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
 
 // Cmd A sub-command that prints a greeting
 func Cmd() *cli.Command {
@@ -12,7 +23,13 @@ func Cmd() *cli.Command {
 		Name:  "do",
 		Usage: "Runs a command",
 		Action: func(c *cli.Context) error {
-			fmt.Println("Run the provided command")
+			out, err := runCommand(c.Args().First(), c.Args().Tail()...)
+
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(string(out))
 			return nil
 		},
 	}
