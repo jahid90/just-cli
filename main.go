99917ce9737d014f9b8384b09fe2ec8ca532a387
Jahid <jahid90@users.noreply.github.com>
2021-08-29 00:25:03 +0530

    colorize errors

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/jahid90/just/cmd"
 	"github.com/jahid90/just/cmd/do"
 	"github.com/urfave/cli/v2"
@@ -34,7 +37,30 @@ func main() {
 	app.Flags = append(app.Flags, do.Cmd().Flags...)
 
 	err := app.Run(os.Args)
+
 	if err != nil {
-		log.Fatal(err)
+
+		splits := strings.Split(err.Error(), ":")
+
+		if len(splits) != 1 {
+			t := splits[0]
+			m := strings.Join(splits[1:], ":")
+
+			// log.Fatal does not respect the color codes
+			if strings.HasPrefix(err.Error(), "info") {
+				info := color.New(color.FgGreen).SprintFunc()
+				fmt.Println(info(t+":") + m)
+			} else if strings.HasPrefix(err.Error(), "warn") {
+				warn := color.New(color.FgYellow).SprintFunc()
+				fmt.Println(warn(t+":") + m)
+				os.Exit(1)
+			} else {
+				alert := color.New(color.FgRed).SprintFunc()
+				fmt.Println(alert(t+":") + m)
+				os.Exit(1)
+			}
+		} else {
+			log.Fatal(err)
+		}
 	}
 }
